bbb967f899770dd7749b9c6ade0d39614f42392b
matheus-marques-esp <matheus.marques.esp@gmail.com>
2024-09-07 14:13:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const { mongoConnect } = require('./controllers/mongoClient');
+
+const indexRoutes = require('./routes/index');
+
+dotenv.config();
+
+process.on('uncaughtException', (err) => {
+    console.error('Unhandled Exception:', err);
+});
+
+mongoConnect();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', indexRoutes);
+
+const port = process.env.PORT || 3010;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
