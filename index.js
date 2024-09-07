7f54783a0d6079faaddf65e4a3fefd1782371dd9
matheus-marques-esp <matheus.marques.esp@gmail.com>
2024-09-07 14:31:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-const { mongoConnect } = require('./controllers/mongoClient');
+const { mongoConnect } = require('./mongoClient');
 
 const indexRoutes = require('./routes/index');
 
